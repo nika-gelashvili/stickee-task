ff447ca3a521f1eda207a182d1d856ea80acbe0c
nika-gelashvili <nika.gelash@gmail.com>
2022-02-08 02:35:29 +0400

    Implement basic crud into functions into WidgetPackSize model.

--- app/Models/WidgetPackSize.php
+++ app/Models/WidgetPackSize.php
@@ -10,4 +10,62 @@ class WidgetPackSize extends Model
     use HasFactory;
 
     protected $table = 'widget_pack_sizes';
+
+    /**
+     * Create new database entry
+     *
+     * @param $size
+     * @return bool
+     */
+    public static function saveNewSize($size): bool
+    {
+        $widgetPackSize = new WidgetPackSize();
+        return self::saveWidgetPackSize($widgetPackSize, $size);
+    }
+
+    /**
+     * Update database entry based on id
+     *
+     * @param $id
+     * @param $size
+     * @return mixed
+     */
+    public static function updateSize($id, $size)
+    {
+        $widgetPackSize = WidgetPackSize::find($id);
+        return self::saveWidgetPackSize($widgetPackSize, $size);
+    }
+
+    /**
+     * Delete database entry based on id
+     *
+     * @param $id
+     * @return mixed
+     */
+    public static function deleteSize($id)
+    {
+        $widgetPackSize = WidgetPackSize::find($id);
+        return $widgetPackSize->delete();
+    }
+
+    /**
+     * Get widgets for table
+     *
+     * @return mixed
+     */
+    public static function getWidgetPackSizeData()
+    {
+        return WidgetPackSize::orderBy('size')->get();
+    }
+
+    /**
+     * @param WidgetPackSize $widgetPackSize
+     * @param $size
+     * @return bool
+     */
+    private static function saveWidgetPackSize(WidgetPackSize $widgetPackSize, $size): bool
+    {
+        $widgetPackSize->size = $size;
+        return $widgetPackSize->save();
+    }
 }
