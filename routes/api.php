e7213b17ba6038bec27ad4a4baa9fbbbedbc6de4
nika-gelashvili <nika.gelash@gmail.com>
2022-02-08 02:39:13 +0400

    Implement routing for crud endpoints.

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/saveWidgetPackSize', 'WidgetController@saveWidgetPackSize');
+Route::post('/updateWidgetPackSize', 'WidgetController@updateWidgetPackSize');
+Route::post('/deleteWidgetPackSize', 'WidgetController@deleteWidgetPackSize');
+Route::get('/getWidgetPackSizesData', 'WidgetController@getWidgetPackSizesData');
