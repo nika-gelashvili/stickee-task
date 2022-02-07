9fb0a946260b751c15d76da80e1da2e069547d51
nika-gelashvili <nika.gelash@gmail.com>
2022-02-08 02:38:23 +0400

    Implement crud endpoints into WidgetController.php

--- app/Http/Controllers/WidgetController.php
+++ app/Http/Controllers/WidgetController.php
@@ -2,9 +2,75 @@
 
 namespace App\Http\Controllers;
 
-
+use App\Models\WidgetPackSize;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class WidgetController extends Controller
 {
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     * @throws ValidationException
+     */
+    public function saveWidgetPackSize(Request $request): JsonResponse
+    {
+        $this->validate($request, [
+            'size' => 'required|numeric|min:1'
+        ]);
 
+        $widgetSaved = WidgetPackSize::saveNewSize($request->size);
+
+        return response()->json([
+            'saved' => $widgetSaved,
+        ]);
+    }
+
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     * @throws ValidationException
+     */
+    public function updateWidgetPackSize(Request $request): JsonResponse
+    {
+        $this->validate($request, [
+            'size' => 'required|numeric|min:1',
+            'id' => 'required|exists:widget_pack_sizes'
+        ]);
+
+        $widgetSaved = WidgetPackSize::updateSize($request->id, $request->size);
+
+        return response()->json([
+            'saved' => $widgetSaved,
+        ]);
+    }
+
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     * @throws ValidationException
+     */
+    public function deleteWidgetPackSize(Request $request): JsonResponse
+    {
+        $this->validate($request, [
+            'id' => 'required|exists:widget_pack_sizes'
+        ]);
+
+        $widgetDeleted = WidgetPackSize::deleteSize($request->id);
+
+        return response()->json([
+            'deleted' => $widgetDeleted,
+        ]);
+    }
+
+    /**
+     * @return JsonResponse
+     */
+    public function getWidgetPackSizesData(): JsonResponse
+    {
+        return response()->json([
+            'data' => WidgetPackSize::getWidgetPackSizeData(),
+        ]);
+    }
 }
